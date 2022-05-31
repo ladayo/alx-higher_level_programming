a8cab55b5e2aad125d4e9a68b7cc43e330915ad3
DayoVagrantVM <georgeoladayo@gmail.com>
2022-05-31 02:01:38 +0000

    task 10

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,27 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
 
+#include <stdlib.h>
 /**
- * check_cycle - checks if a singly linked list has a cycle in it
- * @list: list to be checked
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Return: 0 if no cycle and 1 if cycle
+ * Description: singly linked list node structure
  *
  */
-int check_cycle(listint_t *list)
+typedef struct listint_s
 {
-	listint_t *first, *last = list;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
-	if (list == NULL)
-		return (0);
-	first = list->next;
-	while (last != NULL && first != NULL && first->next != NULL)
-	{
-		if (last == first)
-			return (1);
-		first = first->next->next;
-		last = last->next;
-	}
-	return (0);
-}
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
