edcd82f5608a612903610a02cadeaab63b1e2f52
DayoVagrantVM <georgeoladayo@gmail.com>
2022-06-04 10:12:50 +0000

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string_new = []
-    if my_string:
-        for i in my_string:
-            if i not in ['c', 'C']:
-                my_string_new.append(i)
-        return "".join(my_string_new)
+    my_string_new = [x for x in my_string if x not in ['c', 'C']]
+    return "".join(my_string_new)
