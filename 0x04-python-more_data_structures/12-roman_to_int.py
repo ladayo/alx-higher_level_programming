4662fce8fe8e7c1e8d72da92977be918db9c2a96
DayoVagrantVM <georgeoladayo@gmail.com>
2022-06-08 20:07:45 +0000

    task 12 correction2

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string.isalpha() is not True or roman_string is None:
+    if isinstance(roman_string,str) is not True or roman_string is None:
         return 0
     else:
         roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
