7adb8780bc467f4238c11f7473861ac1f5ef8e17
DayoVagrantVM <georgeoladayo@gmail.com>
2022-06-08 19:47:53 +0000

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string.isalpha() is not True or roman_string is None:
+        return 0
+    else:
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                      'C': 100, 'D': 500, 'M': 1000}
+        roman_value = 0
+        for i in roman_string:
+            if i == 'I' and roman_string.index(i) == 0:
+                roman_v = roman_dict[roman_string[len(roman_string) - 1]]
+                for j in range(0, len(roman_string) - 1):
+                    roman_v = roman_v - roman_dict[roman_string[j]]
+                return (roman_v)
+            else:
+                roman_value = roman_value + roman_dict[i]
+        return (roman_value)
