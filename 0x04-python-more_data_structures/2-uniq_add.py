cfd46072fea26fcbd4c7870192ec651c1508131d
DayoVagrantVM <georgeoladayo@gmail.com>
2022-06-08 12:44:56 +0000

    task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
-        new_list = list({i for i in my_list})
-        add = 0
-        for i in new_list:
-            add = add + i
-        return add
+    new_list = list({i for i in my_list})
+    add = 0
+    for i in new_list:
+        add = add + i
+    return add
