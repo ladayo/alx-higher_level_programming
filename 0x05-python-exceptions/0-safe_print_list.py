e263819ed0ebac072585beb0664aac4e58c18569
DayoVagrantVM <georgeoladayo@gmail.com>
2022-06-20 17:58:44 +0000

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+def safe_print_list(my_list=[], x=0):
+    y = 0
+    new_list = []
+    try:
+        for i in my_list:
+            if y < x:
+                print(i, end="")
+                y += 1
+        print()
+        return (y)
+    except IndexError:
+        for i in my_list:
+            print(i, end="")
+            y += 1
+        print()
+        return (y)
