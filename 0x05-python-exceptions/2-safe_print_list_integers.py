cad443f32dc1121272207b58cafdf412256542a0
DayoVagrantVM <georgeoladayo@gmail.com>
2022-06-20 20:11:48 +0000

    task 2 with other exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
                     y += 1
         print()
         return (y)
-    except IndexError:
+    except (IndexError, ValueError, TypeError):
         for i in my_list:
             if (isinstance(i, int)):
                 print("{:d}".format(i), end="")
