ba4ad8103a88cf6da65e2acaff03fbdcdcd847ee
DayoVagrantVM <georgeoladayo@gmail.com>
2022-06-20 20:01:58 +0000

    ya

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    y = 0
+    try:
+        for i in my_list:
+            if (isinstance(i, int)):
+                if y < x:
+                    print("{:d}".format(i), end="")
+                    y += 1
+        print()
+        return (y)
+    except IndexError:
+        for i in my_list:
+            if (isinstance(i, int)):
+                print(i, end="")
+                y += 1
+        print()
+        return (y)
