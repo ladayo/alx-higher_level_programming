faa29f368252a4a17414c62853a49100a0ddda45
DayoVagrantVM <georgeoladayo@gmail.com>
2022-06-20 22:33:17 +0000

    task 3 try 2

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,8 @@ def safe_print_division(a, b):
     res = None
     try:
         res = a / b
-        return (res)
     except ZeroDivisionError:
         return (res)
     finally:
         print("Inside result {}".format(res))
+        return (res)
