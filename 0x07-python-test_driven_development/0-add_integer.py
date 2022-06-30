e53131cecd2ab153739c866052ab991a563dcdd7
DayoVagrantVM <georgeoladayo@gmail.com>
2022-06-30 00:08:11 +0000

    pycode changes

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,6 +2,8 @@
 """
 module to add two integers
 """
+
+
 def add_integer(a, b=98):
     """
     function to add two integers
@@ -10,9 +12,9 @@ def add_integer(a, b=98):
         a is an integer
         b is an integer
     """
-    if (isinstance(a, int) == False) and (isinstance(a, float) == False):
+    if (isinstance(a, int) is False) and (isinstance(a, float) is False):
         raise TypeError("a must be an integer")
-    if (isinstance(b, int) == False) and (isinstance(b, float) == False):
+    if (isinstance(b, int) is False) and (isinstance(b, float) is False):
         raise TypeError("b must be an integer")
     if isinstance(a, float):
         a = int(a)
