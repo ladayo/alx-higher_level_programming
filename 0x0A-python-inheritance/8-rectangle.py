cfba9947b483bdf4eb68095506202a16db867dff
DayoVagrantVM <georgeoladayo@gmail.com>
2022-07-05 19:01:27 +0000

    task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    8-rectangle Module
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+        Rectangle class
+    """
+
+    def __init__(self, width, height):
+        """
+            Initializing function
+            Args:
+                width: variable to be initialized
+                height: variable to be initialized
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
