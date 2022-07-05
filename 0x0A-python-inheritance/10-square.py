1fb51dc1c4580c73fbea720c5c9b25c0fc80d3cc
DayoVagrantVM <georgeoladayo@gmail.com>
2022-07-05 19:08:11 +0000

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    10-square Module
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+        Square class
+    """
+
+    def __init__(self, size):
+        """
+            special method for initializing instance variables
+            Args:
+                size: size of square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)  # init parent with size
+        self.__size = size
