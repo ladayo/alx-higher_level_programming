a7c77f8a5480110c862639df7e1e03c0af820f42
DayoVagrantVM <georgeoladayo@gmail.com>
2022-07-09 17:25:52 +0000

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    8-class_to_json Module
+"""
+
+
+def class_to_json(obj):
+    """
+        the dictionary description with simple data structure
+        for JSON serialization of an object
+        Args:
+            obj: initial object
+    """
+    if hasattr(obj, '__dict__'):
+        return obj.__dict__.copy()
+    return {}
