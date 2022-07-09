e1994a2a3f68e9208abe02eb3546a46063d18376
DayoVagrantVM <georgeoladayo@gmail.com>
2022-07-09 11:35:09 +0000

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""To JSON string"""
+import json
+
+def to_json_string(my_obj):
+    """
+        JSON representation of an object (string)
+
+        Args:
+            my_obj: object to convert to JSON
+
+        Return: JSON Representation
+    """
+    return(json.dumps(my_obj))
